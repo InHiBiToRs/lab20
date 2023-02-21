483f02a6081e97d1ec65995daa7c374851339785
InHiBiToRs <tanakrit64566@gmail.com>
2023-02-21 17:43:44 +0700

    lab20_2_complete

--- lab20_2.cpp
+++ lab20_2.cpp
@@ -5,8 +5,12 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *A,Rect *B){
+	double x = min((A->x+A->w),(B->x+B->w))-max(A->x,B->x);
+	double y = min((A->y+A->h),(B->y+B->h))-B->y;
+	if(x>0 && y>0){
+		return x*y;
+	}else{
+		return 0;
+	}
 }
